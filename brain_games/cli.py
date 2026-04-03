4567a9c543aecaad9b2217f32bf4e47e507c5a99
Rasim <neutron-1985@live.ru>
2026-04-03 09:23:12 +0000

    Code Quality

---
+++ brain_games/cli.py
@@ -0,0 +1,7 @@
+import prompt
+
+
+def welcome_user(text):
+    name = prompt.string(text)
+    print(f'Hello, {name}!')
+    return name
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,10 @@
+from brain_games.cli import welcome_user
+
+
 def main():
     print("Welcome to the Brain Games!")
+    welcome_user('May I have your name? ')
+
 
 if __name__ == '__main__':
     main()
